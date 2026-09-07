a72be03790c0fbe7c529a5227c8bbd2b30e61c00
sampsm2 <sampsm2@student.eit.ac.nz>
2026-09-08 09:02:24 +1200

    final to privacy
    added content to privacy

--- research/components/privacy.js
+++ research/components/privacy.js
@@ -20,11 +20,32 @@ export const privacy = {
 
                 <div class="flip-card-back position-absolute w-100 h-100 p-3 rounded overflow-y-auto">
 
-                    <p>Privacy & Privacy Policy:  describe what is a privacy policy, 
-                        13 principles with image as illustration? Create a short privacy policy to your website.
-                        Analyze the potential of privacy problems with your project-1 website? How do you handle it?
+                    <p>
+						A privacy policy is a statement or legal document that discloses the ways a party (in this case, the operator of a website) gathers, uses, discloses, and manages a customer, client, or user's data. 
                     </p>
-
+					<p>
+						The 13 Principles of the <a href="https://www.privacy.org.nz/privacy-principles/">Privacy Act 2020</a> are as follows:
+						<ul>
+							<li><a href="https://www.privacy.org.nz/privacy-principles/1/">Principle 1: Purpose for Collection</a></li>
+							<li><a href="https://www.privacy.org.nz/privacy-principles/2/">Principle 2: Source of information - collection from the individual</a></li>
+							<li><a href="https://www.privacy.org.nz/privacy-principles/3/">Principle 3: What to tell the individual when you collect their information directly</a></li>
+							<li><a href="https://www.privacy.org.nz/privacy-principles/3a/">Principle 3A: What to tell the individual when you collect their information from another source</a></li>
+							<li><a href="https://www.privacy.org.nz/privacy-principles/4/">Principle 4: Manner of collection</a></li>
+							<li><a href="https://www.privacy.org.nz/privacy-principles/5/">Principle 5: Storage and security of information</a></li>
+							<li><a href="https://www.privacy.org.nz/privacy-principles/6/">Principle 6: Providing people access to their information</a></li>
+							<li><a href="https://www.privacy.org.nz/privacy-principles/7/">Principle 7: Correction of personal information</a></li>
+							<li><a href="https://www.privacy.org.nz/privacy-principles/8/">Principle 8: Ensure accuracy before using information</a></li>
+							<li><a href="https://www.privacy.org.nz/privacy-principles/9/">Principle 9: Limits on retention of personal information</a></li>
+							<li><a href="https://www.privacy.org.nz/privacy-principles/11/">Principle 10: Use of personal information</a></li>
+							<li><a href="https://www.privacy.org.nz/privacy-principles/11/">Principle 11: Disclosing personal information</a></li>
+							<li><a href="https://www.privacy.org.nz/privacy-principles/12/">Principle 12: Disclosure outside New Zealand</a></li>
+							<li><a href="https://www.privacy.org.nz/privacy-principles/13/">Principle 13: Unique identifiers</a></li>
+						</ul>
+					</p>
+					<p>
+						The major privacy problem with this website is the fact it sends unencrypted data to Supabase, and while it is currently only an address, which can be deliberately mis-inputted, 
+						other websites might use email or phone numbers, inviting spam calls or mail, or even passwords and bank details.
+					</p>
                 </div>
 
             </div>
