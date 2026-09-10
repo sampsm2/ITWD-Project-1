ff29a93487d41d03df0560e6338be5b0afc404e6
sampsm2 <sampsm2@student.eit.ac.nz>
2026-09-11 09:14:44 +1200

    final to hosting
    added content to hosting

--- research/components/hosting.js
+++ research/components/hosting.js
@@ -20,7 +20,21 @@ export const hosting = {
 
                 <div class="flip-card-back position-absolute w-100 h-100 p-3 rounded overflow-y-auto">
 
-                    <p>describe at least 5 major factors you should consider when choosing a right web hosting provider. What web hosting you will choose to publish your project-1 website, and why?
+                    <p>
+                        5 major factors to consider when choosing a hosting provider are:
+                        <ul>
+                            <li>Performance is an important area to consider, a badly performing website results in poor visitor experience. Part of this is <strong>uptime</strong>, a host that guarantees 
+                                99.9% or greater uptime will keep the site accessible year-round. Another part is **speed**, as a slow website will result in frustrated users backing out 
+                                before viewing the whole thing.</li>
+                            <li>Security is another field that is important, especially on sites that operate with user data. <strong>Encryption and malware protection</strong> will keep user data safe, and <strong>automated 
+                                backups</strong> can help keep your website operational if the worst happens and it gets taken down by an attack.</li>
+                            <li>The <strong>level of support from the hosting provider</strong> is also an important consideration, as any form of unplanned downtime or other technical issue can result in lost visits, 
+                                so minimising the time it takes to solve is important</li>
+                        </ul>
+                    </p>
+                    <p>
+                        This site will be published via GitHub as it's basically just the front end, with the back end provided by Supabase. Uptime on a service as widely-used as GitHub Pages should be 
+                        excellent, and support is widely available both from administrators and community members via forums.
                     </p>
 
                 </div>
