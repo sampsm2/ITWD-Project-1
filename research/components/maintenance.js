f8385cc14b930596cef1899960f25b2c178f7a65
sampsm2 <sampsm2@student.eit.ac.nz>
2026-09-11 11:00:36 +1200

    final to maintenance
    added content to maintenance

--- research/components/maintenance.js
+++ research/components/maintenance.js
@@ -20,7 +20,25 @@ export const maintenance = {
 
                 <div class="flip-card-back position-absolute w-100 h-100 p-3 rounded overflow-y-auto">
 
-                    <p>describe at least 5 ways to reduce page loading time & describe 5 important actions (tasks) to carry out when maintaining a website. Test the loading time of your project-1 website? What can you do with your project-1 website to improve its performance?
+                    <p>
+                        5 ways to improve page loading times are:
+                        <ul>
+                            <li><strong>Optimise assets</strong> - Reducing the filesize of images with compression, reduction in resolution, and minimising the number of assets the page has to load.</li>
+                            <li><strong>Minimise render-blocking scripts</strong> - Unnecessary code that loads before more important page content, or scripted elements loaded from somewhere else, can block the page loading and slow down load times.</li>
+                            <li><strong>Limit redirect usage</strong> - Redirects, depending on the loading time of the page, can add fractions of a second to whole seconds to load times.</li>
+                            <li><strong>Minify file size</strong> - Minifying code by removing any code not required to run the script, including comments and whitespace, can gain marginal improvements, but is usually considered a best practise.</li>
+                            <li><strong>Choose effective external services for important functions</strong> - Choosing your server provider, location, and security can minimise user downtime between pages loading.</li>
+                        </ul>
+                    </p>
+                    <p>
+                        5 important tasks when maintaining a website are:
+                        <ul>
+                            <li><strong>Monitor website uptime</strong> - detecting uptime issues early is important for preventing unplanned downtime for extended periods.</li>
+                            <li><strong>Monitor security alerts and vulnerabilities</strong> - to keep the site's security up to scratch and make changes early and efficiently.</li>
+                            <li><strong>Test key functionality</strong> - making sure important site features continue to work correctly, such as forms and external API connections.</li>
+                            <li><strong>Check website performance</strong> - ensuring updates executed by the host or other provider, or by an internal team, aren't affecting performance.</li>
+                            <li><strong>Ensure backups are working</strong> - important to keep backups available if the site needs to be restored due to a security or technical failure.</li>
+                        </ul>
                     </p>
 
                 </div>
