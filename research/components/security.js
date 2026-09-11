2254a12e059879051862cebec058daf33b262bbe
sampsm2 <sampsm2@student.eit.ac.nz>
2026-09-11 12:57:36 +1200

    final to security
    added content to security

--- research/components/security.js
+++ research/components/security.js
@@ -22,6 +22,20 @@ export const security = {
 
                     <p>describe 5 common cyber attacks (threats) and preventive measures against those attacks. Analyze the potential threats of your project-1 website and explain why?
                     </p>
+                    <p>5 common cyber attacks and preventative measures are:
+                        <ul>
+                            <li><strong>Denial-of-service (DoS) and Distributed-denial-of-service (DDoS)</strong> - executed by flooding a server, application, or network with junk traffic to cause lag or crashes, denying service to legitimate users.<br>
+                                Web-application firewalls and DDoS migration services can help to mitigate the effects or block junk traffic.</li>
+                            <li><strong>SQL injection attack</strong> - a common attack that can affect any web database that allows user inputs, that can allow an attacker to retrieve or destroy data.<br>
+                                A developer can use multiple strategies, such as using stored procedures or prepared inputs, or escaping user inputs. Web-application firewalls can also block SQL injection attacks.</li>
+                            <li><strong>Cross-site scripting and cross-site request forgery</strong> - targeting browser activity, this is when an attacker attaches malicious code to a website, that executes when the website is loaded by a client or user.<br>
+                                Web-application firewalls can block many types of these attacks, developers should keep these sorts of attacks in mind when building sites.</li>
+                            <li><strong>Supply chain attack</strong> - an indirect attack against third-party application dependencies, like integrated application or APIs, third-party software, open-source code, or hardware and software vendors.<br>
+                                Supply chain attacks are very difficult to defend against, as it compromises a trusted party. Implementing zero-trust security, running third-party assessments, applying vulnerability patches ASAP, or loading third-party code outside the user's browser, can be effective ways to defend against this type of attack.</li>
+                            <li><strong>On-path attack</strong> - by positioning between two parties that trust each other and impersonating them to each other, an attacker can intercept secure data.<br>
+                                One of the most foundational security measures against on-path attacks is the use of Transport Layer Security (TLS), previously called Secure Socket Layer (SSL), to verify the identity of both parties via a digital signature.</li>
+                        </ul>
+                    </p>
 
                 </div>
 
